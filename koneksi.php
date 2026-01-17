abcc8a9fddba227c863044d4dae5eac62b566c62
dimderoberto-ux <dimderoberto@gmail.com>
2026-01-17 11:12:51 +0800

    crud

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php 
+
+$host = "localhost";
+$port = 
+$user = "root";
+$pass = "";
+$database = "users";
+
+$conn = mysqli_connect($host,$user,$pass,$database);
+
+if(!$conn){
+    die("Koneksi gagal ".mysqli_connect_error());
+}
